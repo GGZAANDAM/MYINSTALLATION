4eec3dbecc222808d9072bd567b7a95098e67319
StellaArtuois <guillermozaanmdam@hotmail.com>
2020-02-10 20:58:40 +0100

    Deleted Tshark

--- MYINSTALLATION.sh
+++ MYINSTALLATION.sh
@@ -23,7 +23,6 @@ sudo apt-get install -y -qq graphviz
 sudo apt-get install -y -qq surfraw surfraw-extra
 sudo apt-get install -y -qq googler
 sudo apt-get install -y -qq jpegoptim
-sudo apt-get install -y -qq tshark
 
 #Snap app(s)
 sudo snap install code --classic
