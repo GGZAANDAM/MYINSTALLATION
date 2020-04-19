6ab1dafe6a3a0dfc8865b5d007e79bf7bf91dc83
StellaArtuois <guillermozaandam@hotmail.com>
2020-04-19 23:05:55 +0200

    OBS Studio

--- MYINSTALLATION.sh
+++ MYINSTALLATION.sh
@@ -16,23 +16,24 @@ sudo apt-get install -y jpegoptim
 sudo apt-get install -y ffmpeg
 sudo apt-get install -y gnome-tweaks
 sudo apt-get install -y gnome-shell-extensions
-sudo apt-get install -y chrome-gnome-shell
 sudo apt-get install -y thunderbird
 sudo apt-get install -y software-properties-common
+sudo apt-get install -y gnome-tweaks
 sudo apt-get install -y gimp
 sudo apt-get install -y scribus
 sudo apt-get install -y krita
 sudo apt-get install -y darktable
 sudo apt-get install -y virt-manager
 sudo apt-get install -y graphviz
+sudo apt-get install -y obs-studio
 sudo apt-get install -y docker
 sudo apt-get install -y tlp
 sudo apt-get install -y zram
 sudo apt-get install -y vagrant
 sudo apt-get install -y clamav clamtk
 sudo apt-get install -y google-chrome-stable
+sudo apt-get install -y chrome-gnome-shell
 sudo apt-get install -y docker
-sudo apt-get install -y gnome-tweaks
 
 sudo apt-get install -y flameshot
 flameshot config -f %T_%F
